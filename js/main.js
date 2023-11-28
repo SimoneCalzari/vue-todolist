0985ae9df09bd7617a69f165e174d2cb703701ca
SimoneCalzari <simone.calzari@outlook.it>
2023-11-28 14:42:53 +0100

    feat: milestone 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      listDos: [
+        {
+          text: 'Fare la spesa',
+          done: false
+        },
+        {
+          text: 'Fare la lavatrice',
+          done: true
+        },
+        {
+          text: 'Studiare Vue',
+          done: false
+        },
+        {
+          text: 'Prendere il gelato',
+          done: true
+        },
+      ]
+    }
+  },
+  methods: {
+
+  }
+}).mount('#app');
